e613a26b096f88572c9fcd5530984c58169cf97d
onyekweremichael <onyekweremichael55@gmail.com>
2025-07-09 07:39:50 +0100

    assesment completed

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -44,6 +44,7 @@ const NavBar = () => {
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
+
           </button>
           {/* Main nav links */}
           <nav className="hidden md:flex gap-3 space-x-6 text-sm">
